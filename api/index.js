500e39c989a01d288e958add33ca556be04c3dc2
polyester_ctrl <shunfreed2016@gmail.com>
2020-06-17 19:46:52 +0900

    mongoose追加

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+var mongoose = require('mongoose');
 // パス指定用モジュール
 const path = require('path');
+
+
+mongoose.connect('mongodb://localhost:27017/userdb', { useNewUrlParser: true });
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  name: String,
+  mail: String,
+  rentbooks: [{ title: String, date: Date }],
+  history: [{ title: String, date: Date }],
+  reserve: [{title: String}]
+})
+var UserModel = mongoose.model('users', UserSchema);
+var user = new UserModel();
+user.name = 'taro';
+user.mail = 'aa@example.com';
+user.save();
 
 // 8080番ポートで待ちうける
 app.listen(8080, () => {
